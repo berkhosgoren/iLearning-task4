91bc8531d617fd9f676dd8e3045f5337bd61c30d
berkhosgoren <berk.2000hosgoren@hotmail.com>
2026-02-09 18:17:33 +0300

    added db context and user model

---
+++ api/Api/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Api.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = null;
+
+        public string Email { get; set; } = null;
+
+        public string PasswordHash { get; set; } = null;
+
+        public bool IsEmailConfirmed { get; set; }
+
+        public UserStatus Status { get; set; }
+
+        public DateTime CreatedAtUtc { get; set; }
+
+        public DateTime? LastLoginAtUtc { get; set; }
+
+    }
+    
+    public enum UserStatus
+    {
+        Unverified = 0,
+        Active = 1,
+        Blocked = 2
+    }
+}
