f98af220d8c3a01b206e33b160ca5523fb409173
berkhosgoren <berk.2000hosgoren@hotmail.com>
2026-02-11 20:01:31 +0300

    added register page and integrated register flow with the API

--- client/client/src/app/features/auth/register/register.ts
+++ client/client/src/app/features/auth/register/register.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
 
+  private auth = inject(AuthService);
+  private router = inject(Router);
+
+  name = '';
+  email = '';
+  password = '';
+  message = '';
+
+  register(){
+    this.auth.register({
+      name: this.name,
+      email: this.email,
+      password: this.password
+    }).subscribe({
+      next: () => {
+        this.message = 'Registered. Confirm your email.';
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        this.message = err.error?.message ?? 'Error';
+      }
+    });
+  }
 }
